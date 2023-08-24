1daefc0a1b6db0944fdbc71ed77d666deff9fed5
rockywalter <51148383+rockywalter@users.noreply.github.com>
2023-08-24 17:50:26 +0530

    create campaigns commented

--- src/views/Campaigns/CreateCampaigns/index.js
+++ src/views/Campaigns/CreateCampaigns/index.js
@@ -1,55 +1,55 @@
-import { CModal, CModalHeader, CModalTitle, CModalBody, CModalFooter, CButton} from '@coreui/react'
-import React, { useState } from 'react';
-import { TagsInput } from "react-tag-input-component";
+// import { CModal, CModalHeader, CModalTitle, CModalBody, CModalFooter, CButton} from '@coreui/react'
+// import React, { useState } from 'react';
+// import { TagsInput } from "react-tag-input-component";
 
-const CreateCampaigns = () => {
+// const CreateCampaigns = () => {
 
-  const [selected, setSelected] = useState(["papaya"]);
-  const [visible, setVisible] = useState(true)
+//   const [selected, setSelected] = useState(["papaya"]);
+//   const [visible, setVisible] = useState(true)
 
-  return (
-    <div>
-
-
-    <CModal visible={visible} onClose={() => setVisible(false)}>
-      <CModalHeader onClose={() => setVisible(false)}>
-        <CModalTitle>Modal title</CModalTitle>
-      </CModalHeader>
-      <CModalBody>
-        <p>Woohoo, you're reading this text in a modal!</p>
-      </CModalBody>
-      <CModalFooter>
-        <CButton color="secondary" onClick={() => setVisible(false)}>
-          Close
-        </CButton>
-        <CButton color="primary">Save changes</CButton>
-      </CModalFooter>
-    </CModal>
+//   return (
+//     <div>
 
 
-
-    <div>
-      <h1>Add Fruits</h1>
-
-      <pre>{JSON.stringify(selected)}</pre>
-
-      <TagsInput
-        value={selected}
-        onChange={setSelected}
-        name="fruits"
-        placeHolder="enter fruits"
-      />
-      <em>press enter to add new tag</em>
-    </div>
-
-
-    </div>
+//     <CModal visible={visible} onClose={() => setVisible(false)}>
+//       <CModalHeader onClose={() => setVisible(false)}>
+//         <CModalTitle>Modal title</CModalTitle>
+//       </CModalHeader>
+//       <CModalBody>
+//         <p>Woohoo, you're reading this text in a modal!</p>
+//       </CModalBody>
+//       <CModalFooter>
+//         <CButton color="secondary" onClick={() => setVisible(false)}>
+//           Close
+//         </CButton>
+//         <CButton color="primary">Save changes</CButton>
+//       </CModalFooter>
+//     </CModal>
 
 
 
+//     <div>
+//       <h1>Add Fruits</h1>
+
+//       <pre>{JSON.stringify(selected)}</pre>
+
+//       <TagsInput
+//         value={selected}
+//         onChange={setSelected}
+//         name="fruits"
+//         placeHolder="enter fruits"
+//       />
+//       <em>press enter to add new tag</em>
+//     </div>
 
 
-  )
-}
+//     </div>
 
-export default CreateCampaigns
+
+
+
+
+//   )
+// }
+
+// export default CreateCampaigns
